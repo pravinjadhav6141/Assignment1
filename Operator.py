ab9698e757a68ec9a000d87492125d781d86df80
pravinjadhav6141 <140793008+pravinjadhav6141@users.noreply.github.com>
2023-07-28 12:50:03 +0530

    Add files via upload
    ok

---
+++ Operator.py
@@ -0,0 +1,92 @@
+'print(5!=2)'
+
+'''i=int(input("Enter input value")) 
+i+=2
+print(i)'''
+
+'''a=4
+print(a>=1 AND a<=20)
+x=[1,2,3,4]
+y=[2,3,4,5]
+print(x != y)
+
+n=int(input("Enter the new value "))
+if(n>0):
+    print("Number is positive ")
+else:
+    print("Number is negativ")
+      
+
+'''
+
+
+'''
+n=int(input("Enter the no"))
+if(n%2==0):   
+   print("No is Even ")
+else:   
+    print("No is Odd")
+'''
+
+''' n=int(input("Enter the no"))
+if(n%3==0 and n%5==0):
+    print(n,"No is divisible by 3 and 5")
+else:
+    print(n, "No is not divisible by 3 and 5")
+    '''
+
+
+'''
+ch=int(input("Enter the below no 4 choices \n 1.n1  even or odd\n 2.n1 divisibility \n 3.n1 is positive or negative \n 4. addition of two no")
+if ch==1:
+          n=int(input("Enter the value")
+          if(n%2==0):
+                print(n,"enter the is positive")
+         else:
+                print(n,"Enter the no is negative ")
+ else if ch==2:
+               n=int(input("Enter the value")
+               n1=int(input("Enter the value")
+             if: n%n1==0:
+                print(n,"no is divisible by 2")
+             else:
+                  print(n,"No is not divisible by 2")
+             
+    '''
+'''
+i=1
+while (i<=50):
+       print(i)
+       i+=3
+'''
+
+'Write a program to accept no from user and calculate its factors'
+'''n=int(input("Enter the number"))
+i=1
+while(i<=n):
+        if n%i==0:
+              print(i)
+              i=i+1'''
+
+'''Introduction to Datatye 
+i=1
+type(i)
+id(i)'''
+
+'String'
+'Collection of charecter'
+s="WELCOME"
+
+print(type(s))
+len(s)
+
+
+
+
+
+
+
+
+
+
+
